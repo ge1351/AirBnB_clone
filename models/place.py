482ac8aa0f90748417b89aad7845a0ba7a095306
Getachew Alemayehu <105641499+ge1351@users.noreply.github.com>
2022-10-30 16:27:36 +0300

    Update place.py

--- models/place.py
+++ models/place.py
@@ -1,35 +1,23 @@
 #!/usr/bin/python3
-""" Subclass of BaseModel in charge of saving the place information"""
+"""Defines the Place class."""
 from models.base_model import BaseModel
 
 
 class Place(BaseModel):
-    """ Class Used to represent an apartment/house/room
-        Attributes
-        ----------
-        city_id: str
-            it will be the City.id
-        user_id: str
-            it will be the User.id
-        name: str
-            name of the place
-        description: str
-            description of the apartment/house/room
-        number_rooms: int
-            number of rooms avaliable
-        number_bathrooms: int
-            number of bathrooms avaliable
-        max_guest: int
-            number of guest the place can have
-        price_by_night: int
-            the price of the night represent with an int
-        latitude: flt
-            the latitude of the place
-        longitude: flt
-            the longitude of the place
-        amenity_ids: list of string
-            it will be the list of Amenity.id later
-            """
+    """Represent a place.
+    Attributes:
+        city_id (str): The City id.
+        user_id (str): The User id.
+        name (str): The name of the place.
+        description (str): The description of the place.
+        number_rooms (int): The number of rooms of the place.
+        number_bathrooms (int): The number of bathrooms of the place.
+        max_guest (int): The maximum number of guests of the place.
+        price_by_night (int): The price by night of the place.
+        latitude (float): The latitude of the place.
+        longitude (float): The longitude of the place.
+        amenity_ids (list): A list of Amenity ids.
+    """
 
     city_id = ""
     user_id = ""
